e890a8df1b763e1bb74b3e8a60fc9a1b21be007a
Egor Gavrilov <gavrilovegor519-2@yandex.ru>
2025-07-05 02:33:01 +0000

    Some more fixes

--- nodehistj-history-diff/src/test/java/ru/gavrilovegor519/nodehistj_history_diff/controller/NodeHistoryControllerTest.java
+++ nodehistj-history-diff/src/test/java/ru/gavrilovegor519/nodehistj_history_diff/controller/NodeHistoryControllerTest.java
@@ -15,28 +15,20 @@ public class NodeHistoryControllerTest extends BaseIntegrationTest {
     private MockMvc mockMvc;
 
     @Test
-    void testGetNodeHistory() throws Exception {
-        mockMvc.perform(get("/history/node/1/1/1"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.content[0].nodeName").value("Test Node"))
-                .andExpect(jsonPath("$.content[0].changeType").value("ADDED"));
-    }
-
-    @Test
     void testGetChangesForDate() throws Exception {
         mockMvc.perform(get("/history/date/2023-01-01"))
                 .andExpect(status().isOk())
-                .andExpected(jsonPath("$[0].nodeName").value("Test Node"))
-                .andExpected(jsonPath("$[0].changeType").value("ADDED"));
+                .andExpect(jsonPath("$[0].nodeName").value("Test Node"))
+                .andExpect(jsonPath("$[0].changeType").value("ADDED"));
     }
 
     @Test
     void testGetChangesByType() throws Exception {
         mockMvc.perform(get("/history/type/MODIFIED"))
                 .andExpect(status().isOk())
-                .andExpected(jsonPath("$.content[0].nodeName").value("Modified Node"))
-                .andExpected(jsonPath("$.content[0].changeType").value("MODIFIED"))
-                .andExpected(jsonPath("$.content[0].prevNodeName").value("Old Node"));
+                .andExpect(jsonPath("$.content[0].nodeName").value("Modified Node"))
+                .andExpect(jsonPath("$.content[0].changeType").value("MODIFIED"))
+                .andExpect(jsonPath("$.content[0].prevNodeName").value("Old Node"));
     }
 
     @Test
@@ -45,7 +37,7 @@ public class NodeHistoryControllerTest extends BaseIntegrationTest {
                         .param("startDate", "2023-01-01")
                         .param("endDate", "2023-01-02"))
                 .andExpect(status().isOk())
-                .andExpected(jsonPath("$[0].addedCount").value(1))
-                .andExpected(jsonPath("$[1].modifiedCount").value(1));
+                .andExpect(jsonPath("$[0].addedCount").value(1))
+                .andExpect(jsonPath("$[1].modifiedCount").value(1));
     }
 }
