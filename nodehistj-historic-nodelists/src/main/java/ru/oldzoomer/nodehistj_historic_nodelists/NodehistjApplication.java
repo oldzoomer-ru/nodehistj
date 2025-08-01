c7c8efe91237949b4e7880456e5db3c07e5722b2
oldzoomer-ru <gavrilovegor519-2@yandex.ru>
2025-08-01 02:32:11 +0000

    Use AspectJ in historic nodelist module

--- nodehistj-historic-nodelists/src/main/java/ru/oldzoomer/nodehistj_historic_nodelists/NodehistjApplication.java
+++ nodehistj-historic-nodelists/src/main/java/ru/oldzoomer/nodehistj_historic_nodelists/NodehistjApplication.java
@@ -2,6 +2,7 @@ package ru.oldzoomer.nodehistj_historic_nodelists;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AdviceMode;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
@@ -9,7 +10,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  * Launches Spring Boot application with enabled transaction management.
  */
 @SpringBootApplication
-@EnableTransactionManagement
+@EnableTransactionManagement(mode = AdviceMode.ASPECTJ)
 public class NodehistjApplication {
 
     /**
