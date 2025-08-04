a9a271cc8e63f1da701b2291a11cc0b891211865
Egor Gavrilov <gavrilovegor519-2@yandex.ru>
2025-08-04 21:58:27 +0300

    Fix for true 404 code using

--- lib/common-utils/src/main/java/ru/oldzoomer/common/utils/ExceptionResolver.java
+++ lib/common-utils/src/main/java/ru/oldzoomer/common/utils/ExceptionResolver.java
@@ -1,14 +1,16 @@
 package ru.oldzoomer.common.utils;
 
-import jakarta.validation.ConstraintViolationException;
-import lombok.extern.slf4j.Slf4j;
+import java.util.NoSuchElementException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
-import java.util.NoSuchElementException;
+import jakarta.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Global exception handler for REST controllers.
@@ -17,7 +19,7 @@ import java.util.NoSuchElementException;
 @Slf4j
 public class ExceptionResolver {
 
-    @ExceptionHandler(NoSuchElementException.class)
+    @ExceptionHandler({NoSuchElementException.class, NoResourceFoundException.class})
     public ResponseEntity<ErrorDto> handleNotFound(NoSuchElementException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body(new ErrorDto("Not Found", ex.getMessage()));
