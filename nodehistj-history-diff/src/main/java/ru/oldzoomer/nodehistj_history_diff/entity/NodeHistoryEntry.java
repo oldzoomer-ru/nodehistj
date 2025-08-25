edc4f74912a77994690ed9f9a468bcdfcee78c80
Egor Gavrilov <gavrilovegor519-2@yandex.ru>
2025-08-25 11:13:00 +0300

    Refactor NodeHistoryEntry to use NodeEntryKey

--- nodehistj-history-diff/src/main/java/ru/oldzoomer/nodehistj_history_diff/entity/NodeHistoryEntry.java
+++ nodehistj-history-diff/src/main/java/ru/oldzoomer/nodehistj_history_diff/entity/NodeHistoryEntry.java
@@ -1,13 +1,10 @@
 package ru.oldzoomer.nodehistj_history_diff.entity;
 
-import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
 import org.springframework.data.cassandra.core.mapping.*;
 
 import java.io.Serializable;
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Objects;
-import java.util.UUID;
 import lombok.Getter;
 import lombok.Setter;
 import ru.oldzoomer.nodelistj.enums.Keywords;
@@ -15,37 +12,12 @@ import ru.oldzoomer.nodelistj.enums.Keywords;
 @Getter
 @Setter
 @Table("node_history_entry")
-public class NodeHistoryEntry {
-    
-    @PrimaryKeyClass
-    @Getter
-    @Setter
-    public static class NodeHistoryEntryKey implements Serializable {
-        @PrimaryKeyColumn(name = "zone", ordinal = 0, type = PrimaryKeyType.PARTITIONED)
-        private Integer zone;
-        
-        @PrimaryKeyColumn(name = "network", ordinal = 1)
-        private Integer network;
-        
-        @PrimaryKeyColumn(name = "node", ordinal = 2)
-        private Integer node;
-        
-        @PrimaryKeyColumn(name = "nodelist_year", ordinal = 3)
-        private Integer nodelistYear;
-        
-        @PrimaryKeyColumn(name = "nodelist_name", ordinal = 4)
-        private String nodelistName;
-        
-        @PrimaryKeyColumn(name = "change_type", ordinal = 5)
-        private ChangeType changeType;
-        
-        @PrimaryKeyColumn(name = "id", ordinal = 6)
-        private UUID id;
-    }
-    
+public class NodeHistoryEntry implements Serializable {
     @PrimaryKey
-    private NodeHistoryEntryKey key;
-    
+    private NodeEntryKey id;
+
+    private ChangeType changeType;
+
     private Keywords keywords;
     private String nodeName;
     private String location;
@@ -60,23 +32,14 @@ public class NodeHistoryEntry {
     private String prevPhone;
     private Integer prevBaudRate;
     private List<String> prevFlags;
-    
-    private LocalDate changeDate;
 
     public NodeHistoryEntry() {
-        this.key = new NodeHistoryEntryKey();
-        this.key.id = UUID.randomUUID();
+        this.id = new NodeEntryKey();
     }
 
     public NodeHistoryEntry(Integer zone, Integer network, Integer node, Integer nodelistYear,
-                           String nodelistName, ChangeType changeType) {
-        this();
-        this.key.zone = zone;
-        this.key.network = network;
-        this.key.node = node;
-        this.key.nodelistYear = nodelistYear;
-        this.key.nodelistName = nodelistName;
-        this.key.changeType = changeType;
+            String nodelistName, ChangeType changeType) {
+        this.id = new NodeEntryKey(zone, network, node, nodelistYear, nodelistName);
     }
 
     @Override
@@ -88,91 +51,15 @@ public class NodeHistoryEntry {
             return false;
         }
         NodeHistoryEntry that = (NodeHistoryEntry) o;
-        return Objects.equals(key, that.key);
+        return Objects.equals(id, that.id);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(key);
+        return Objects.hash(id);
     }
 
     public enum ChangeType {
         ADDED, REMOVED, MODIFIED
-    }
-
-    // Публичные методы доступа к полям ключа для обратной совместимости
-    public Integer getZone() {
-        return key != null ? key.getZone() : null;
-    }
-
-    public Integer getNetwork() {
-        return key != null ? key.getNetwork() : null;
-    }
-
-    public Integer getNode() {
-        return key != null ? key.getNode() : null;
-    }
-
-    public Integer getNodelistYear() {
-        return key != null ? key.getNodelistYear() : null;
-    }
-
-    public String getNodelistName() {
-        return key != null ? key.getNodelistName() : null;
-    }
-
-    public ChangeType getChangeType() {
-        return key != null ? key.getChangeType() : null;
-    }
-
-    // Публичные методы установки полей ключа для обратной совместимости
-    public void setZone(Integer zone) {
-        if (key != null) {
-            key.setZone(zone);
-        }
-    }
-
-    public void setNetwork(Integer network) {
-        if (key != null) {
-            key.setNetwork(network);
-        }
-    }
-
-    public void setNode(Integer node) {
-        if (key != null) {
-            key.setNode(node);
-        }
-    }
-
-    public void setNodelistYear(Integer nodelistYear) {
-        if (key != null) {
-            key.setNodelistYear(nodelistYear);
-        }
-    }
-
-    public void setNodelistName(String nodelistName) {
-        if (key != null) {
-            key.setNodelistName(nodelistName);
-        }
-    }
-
-    public void setChangeType(ChangeType changeType) {
-        if (key != null) {
-            key.setChangeType(changeType);
-        }
-    }
-
-    public void setId(UUID id) {
-        if (key != null) {
-            key.setId(id);
-        }
-    }
-
-    public LocalDate getChangeDate() {
-        return changeDate;
-    }
-
-    public void setChangeDate(LocalDate changeDate) {
-        this.changeDate = changeDate;
     }
 }
