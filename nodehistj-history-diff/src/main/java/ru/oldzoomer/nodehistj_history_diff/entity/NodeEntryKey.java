5730f3a311c51b222b14c206b360ad482e440b89
Egor Gavrilov <gavrilovegor519-2@yandex.ru>
2025-08-25 11:20:49 +0300

    Add NodeEntryKey entity for history diff

---
+++ nodehistj-history-diff/src/main/java/ru/oldzoomer/nodehistj_history_diff/entity/NodeEntryKey.java
@@ -0,0 +1,58 @@
+package ru.oldzoomer.nodehistj_history_diff.entity;
+
+import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
+import org.springframework.data.cassandra.core.mapping.PrimaryKeyClass;
+import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@PrimaryKeyClass
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class NodeEntryKey implements Serializable {
+
+    @PrimaryKeyColumn(name = "zone", type = PrimaryKeyType.PARTITIONED)
+    private Integer zone;
+
+    @PrimaryKeyColumn(name = "network", type = PrimaryKeyType.CLUSTERED)
+    private Integer network;
+
+    @PrimaryKeyColumn(name = "node", type = PrimaryKeyType.CLUSTERED)
+    private Integer node;
+
+    @PrimaryKeyColumn(name = "nodelist_year", type = PrimaryKeyType.CLUSTERED)
+    private Integer nodelistYear;
+
+    @PrimaryKeyColumn(name = "nodelist_name", type = PrimaryKeyType.CLUSTERED)
+    private String nodelistName;
+
+    // Equals and HashCode
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        NodeEntryKey that = (NodeEntryKey) o;
+        return Objects.equals(nodelistYear, that.nodelistYear) &&
+                Objects.equals(nodelistName, that.nodelistName) &&
+                Objects.equals(zone, that.zone) &&
+                Objects.equals(network, that.network) &&
+                Objects.equals(node, that.node);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nodelistYear, nodelistName, zone, network, node);
+    }
+}
