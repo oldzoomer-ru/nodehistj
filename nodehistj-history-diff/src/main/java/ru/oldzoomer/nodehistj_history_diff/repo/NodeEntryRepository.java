2cf0e305287e8afb9d4802bfac01699796ae8da1
Egor Gavrilov <gavrilovegor519-2@yandex.ru>
2025-08-30 14:54:36 +0300

    Improve NodeEntryRepository interface with comprehensive documentation

--- nodehistj-history-diff/src/main/java/ru/oldzoomer/nodehistj_history_diff/repo/NodeEntryRepository.java
+++ nodehistj-history-diff/src/main/java/ru/oldzoomer/nodehistj_history_diff/repo/NodeEntryRepository.java
@@ -8,14 +8,30 @@ import org.springframework.data.jpa.repository.Query;
 
 import ru.oldzoomer.nodehistj_history_diff.entity.NodeEntry;
 
+/**
+ * Repository interface for NodeEntry entities.
+ * Provides methods to find NodeEntry entities based on various criteria.
+ */
 public interface NodeEntryRepository extends JpaRepository<NodeEntry, Long> {
 
+    /**
+     * Finds NodeEntry entities based on nodelist year and name.
+     *
+     * @param nodelistYear the year of the nodelist
+     * @param nodelistName the name of the nodelist
+     * @return a list of NodeEntry entities matching the criteria
+     */
     @EntityGraph("NodeEntry.nodelistEntry")
     @Query("SELECT ne FROM NodeEntry ne " +
            "WHERE ne.nodelistEntry.nodelistYear = ?1 " +
            "AND ne.nodelistEntry.nodelistName = ?2")
     List<NodeEntry> findByNodelist(Integer nodelistYear, String nodelistName);
 
+    /**
+     * Finds all nodelist versions.
+     *
+     * @return a list of Object arrays containing nodelist year and name
+     */
     @Query("SELECT DISTINCT ne.nodelistEntry.nodelistYear, ne.nodelistEntry.nodelistName " +
             "FROM NodeEntry ne " +
             "ORDER BY ne.nodelistEntry.nodelistYear DESC, ne.nodelistEntry.nodelistName DESC")
