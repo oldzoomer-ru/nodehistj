f040e507ae7fb308599a3ede694c391b0cff47d7
Egor Gavrilov <gavrilovegor519-2@yandex.ru>
2025-08-30 14:15:21 +0300

    Add documentation to NoNewObjects exception class

--- nodehistj-newest-nodelists/src/main/java/ru/oldzoomer/nodehistj_newest_nodelists/exception/NoNewObjects.java
+++ nodehistj-newest-nodelists/src/main/java/ru/oldzoomer/nodehistj_newest_nodelists/exception/NoNewObjects.java
@@ -1,10 +1,26 @@
 package ru.oldzoomer.nodehistj_newest_nodelists.exception;
 
+/**
+ * Exception thrown when no new objects are found during processing.
+ * <p>
+ * This exception indicates that the system did not find any new objects
+ * that needed to be processed, which might be a normal condition or
+ * could indicate a problem depending on the context.
+ */
 public class NoNewObjects extends RuntimeException {
+    /**
+     * Constructs a new NoNewObjects exception with a default message.
+     */
     public NoNewObjects() {
         super("No new objects found");
     }
 
+    /**
+     * Constructs a new NoNewObjects exception with a default message
+     * and the specified cause.
+     *
+     * @param cause the cause of this exception
+     */
     public NoNewObjects(Throwable cause) {
         super("No new objects found", cause);
     }
