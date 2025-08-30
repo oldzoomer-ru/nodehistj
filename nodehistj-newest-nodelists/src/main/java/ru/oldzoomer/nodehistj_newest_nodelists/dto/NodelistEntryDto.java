5e0f6db89e986303da6254376caff7a05b72f4d9
Egor Gavrilov <gavrilovegor519-2@yandex.ru>
2025-08-30 14:54:37 +0300

    Add comprehensive documentation to NodelistEntryDto class

--- nodehistj-newest-nodelists/src/main/java/ru/oldzoomer/nodehistj_newest_nodelists/dto/NodelistEntryDto.java
+++ nodehistj-newest-nodelists/src/main/java/ru/oldzoomer/nodehistj_newest_nodelists/dto/NodelistEntryDto.java
@@ -5,7 +5,8 @@ import java.io.Serializable;
 import lombok.Data;
 
 /**
- * DTO for {@link ru.oldzoomer.nodehistj_newest_nodelists.entity.NodelistEntry}
+ * DTO for {@link ru.oldzoomer.nodehistj_newest_nodelists.entity.NodelistEntry}.
+ * Contains information about a specific nodelist.
  */
 @Data
 public class NodelistEntryDto implements Serializable {
