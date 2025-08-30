9f334644fc2ecc16f962a8804e946e50f54dba36
Egor Gavrilov <gavrilovegor519-2@yandex.ru>
2025-08-30 14:15:21 +0300

    Add documentation to NodeEntryKey entity class

--- nodehistj-historic-nodelists/src/main/java/ru/oldzoomer/nodehistj_historic_nodelists/entity/NodeEntryKey.java
+++ nodehistj-historic-nodelists/src/main/java/ru/oldzoomer/nodehistj_historic_nodelists/entity/NodeEntryKey.java
@@ -12,6 +12,26 @@ import lombok.Setter;
 import java.io.Serializable;
 import java.util.Objects;
 
+/**
+ * Composite primary key for NodeEntry entity.
+ * <p>
+ * Represents the unique identifier for a node entry in the system, combining:
+ * - Nodelist year
+ * - Nodelist name
+ * - Zone identifier
+ * - Network identifier
+ * - Node identifier
+ * <p>
+ * This key is used to uniquely identify a node entry in the database.
+ * <p>
+ * The key structure follows Cassandra's data modeling best practices:
+ * - Nodelist year and name are used as partition keys for efficient data distribution
+ * - Zone, network, and node are used as clustering columns to
+ *   organize data within partitions
+ * <p>
+ * This composite key allows for efficient querying of node entries by
+ * different combinations of year, name, zone, network, and node.
+ */
 @PrimaryKeyClass
 @Getter
 @Setter
