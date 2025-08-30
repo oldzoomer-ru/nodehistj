9d8adbd8d2ab8fcd77739734c2dc4723910595b5
Egor Gavrilov <gavrilovegor519-2@yandex.ru>
2025-08-30 14:15:21 +0300

    Add documentation to NodeEntryKeyDto class

--- nodehistj-newest-nodelists/src/main/java/ru/oldzoomer/nodehistj_newest_nodelists/dto/NodeEntryKeyDto.java
+++ nodehistj-newest-nodelists/src/main/java/ru/oldzoomer/nodehistj_newest_nodelists/dto/NodeEntryKeyDto.java
@@ -7,6 +7,25 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
+/**
+ * DTO for NodeEntryKey entity.
+ * <p>
+ * Represents the composite primary key for a node entry in the system, containing:
+ * - Nodelist year
+ * - Nodelist name
+ * - Zone identifier (1-6)
+ * - Network identifier (1-32768)
+ * - Node identifier (1-32768)
+ * <p>
+ * This DTO is used for transferring node entry key information between layers.
+ * <p>
+ * Validation constraints ensure that:
+ * - Zone must be between 1 and 6
+ * - Network must be between 1 and 32768
+ * - Node must be between 1 and 32768
+ * <p>
+ * The constraints reflect the Fidonet addressing standards (FTS-0005).
+ */
 @Data
 public class NodeEntryKeyDto implements Serializable {
     private Integer nodelistYear;
