be15c73273567767ece39b7cb0d3b9d722a4b861
Egor Gavrilov <gavrilovegor519-2@yandex.ru>
2025-08-30 14:15:21 +0300

    Add documentation to CassandraCreateKeyspaceAutoConfiguration class

--- nodehistj-newest-nodelists/src/main/java/ru/oldzoomer/nodehistj_newest_nodelists/config/CassandraCreateKeyspaceAutoConfiguration.java
+++ nodehistj-newest-nodelists/src/main/java/ru/oldzoomer/nodehistj_newest_nodelists/config/CassandraCreateKeyspaceAutoConfiguration.java
@@ -15,12 +15,26 @@ import com.datastax.oss.driver.api.core.CqlSessionBuilder;
 
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * Configuration class for creating Cassandra keyspace.
+ * <p>
+ * Responsible for:
+ * - Creating keyspace if it doesn't exist
+ * - Configuring Cassandra connection properties
+ * - Setting up keyspace creation conditions
+ */
 @Configuration
 @ConditionalOnClass(CqlSession.class)
 @ConditionalOnProperty(name = "spring.cassandra.create-keyspace", havingValue = "true")
 @AutoConfigureBefore(CassandraAutoConfiguration.class)
 @Slf4j
 public class CassandraCreateKeyspaceAutoConfiguration {
+    /**
+     * Constructor for CassandraCreateKeyspaceAutoConfiguration.
+     *
+     * @param cqlSessionBuilder CQL session builder
+     * @param properties Cassandra properties
+     */
     public CassandraCreateKeyspaceAutoConfiguration(
             CqlSessionBuilder cqlSessionBuilder,
             CassandraProperties properties) {
