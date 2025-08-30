45184a9bd222e41186f4455cfba4c647e07e40df
Egor Gavrilov <gavrilovegor519-2@yandex.ru>
2025-08-30 14:54:37 +0300

    Add comprehensive documentation to NodelistEntry class

--- nodehistj-newest-nodelists/src/main/java/ru/oldzoomer/nodehistj_newest_nodelists/entity/NodelistEntry.java
+++ nodehistj-newest-nodelists/src/main/java/ru/oldzoomer/nodehistj_newest_nodelists/entity/NodelistEntry.java
@@ -13,6 +13,10 @@ import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * Entity class representing a nodelist entry in the database.
+ * Contains information about a specific nodelist.
+ */
 @Getter
 @Setter
 @Entity
@@ -30,6 +34,13 @@ public class NodelistEntry implements Serializable {
     @Column(name = "nodelist_name")
     private String nodelistName;
 
+    /**
+     * Compares this NodelistEntry with another object for equality.
+     * Two NodelistEntry objects are considered equal if they have the same ID.
+     *
+     * @param o the object to compare with
+     * @return true if the objects are equal, false otherwise
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -41,6 +52,12 @@ public class NodelistEntry implements Serializable {
         return Objects.equals(id, that.id);
     }
 
+    /**
+     * Returns a hash code value for this NodelistEntry.
+     * The hash code is based on the ID of the NodelistEntry.
+     *
+     * @return a hash code value for this NodelistEntry
+     */
     @Override
     public int hashCode() {
         return Objects.hashCode(id);
