991e6e8596d3d7b642e8b566ee7bdbd15770feb0
Egor Gavrilov <gavrilovegor519-2@yandex.ru>
2025-08-30 14:54:36 +0300

    Add comprehensive documentation to NodeHistoryEntryMapper interface

--- nodehistj-history-diff/src/main/java/ru/oldzoomer/nodehistj_history_diff/mapper/NodeHistoryEntryMapper.java
+++ nodehistj-history-diff/src/main/java/ru/oldzoomer/nodehistj_history_diff/mapper/NodeHistoryEntryMapper.java
@@ -9,8 +9,25 @@ import org.mapstruct.ReportingPolicy;
 import ru.oldzoomer.nodehistj_history_diff.dto.NodeHistoryEntryDto;
 import ru.oldzoomer.nodehistj_history_diff.entity.NodeHistoryEntry;
 
+/**
+ * Mapper interface for converting NodeHistoryEntry entities to NodeHistoryEntryDto objects.
+ * Uses Spring component model and ignores unmapped target properties.
+ */
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
 public interface NodeHistoryEntryMapper {
+    /**
+     * Converts a list of NodeHistoryEntry entities to a list of NodeHistoryEntryDto objects.
+     *
+     * @param nodeHistoryEntries the list of NodeHistoryEntry entities to convert
+     * @return the list of converted NodeHistoryEntryDto objects
+     */
     List<NodeHistoryEntryDto> toDto(List<NodeHistoryEntry> nodeHistoryEntries);
+
+    /**
+     * Converts a single NodeHistoryEntry entity to a NodeHistoryEntryDto object.
+     *
+     * @param nodeHistoryEntry the NodeHistoryEntry entity to convert
+     * @return the converted NodeHistoryEntryDto object
+     */
     NodeHistoryEntryDto toDto(NodeHistoryEntry nodeHistoryEntry);
 }
