a82232fa894f97e3e30bca1ec924b516a6b7beb9
Egor Gavrilov <gavrilovegor519-2@yandex.ru>
2025-08-30 14:15:20 +0300

    Add documentation to MinioConfig class

--- lib/minio/src/main/java/ru/oldzoomer/minio/config/MinioConfig.java
+++ lib/minio/src/main/java/ru/oldzoomer/minio/config/MinioConfig.java
@@ -6,9 +6,23 @@ import org.springframework.context.annotation.Configuration;
 
 import io.minio.MinioClient;
 
+/**
+ * Configuration class for MinIO client.
+ * <p>
+ * Responsible for:
+ * - Creating MinIO client instance
+ * - Configuring MinIO connection properties
+ * - Setting up MinIO client with credentials
+ */
 @Configuration
 @EnableConfigurationProperties(MinioConnectionProperties.class)
 public class MinioConfig {
+    /**
+     * Creates a MinIO client instance.
+     *
+     * @param minioConnectionProperties MinIO connection properties
+     * @return MinioClient instance configured with connection properties
+     */
     @Bean
     MinioClient minioClient(MinioConnectionProperties minioConnectionProperties) {
         return MinioClient.builder()
