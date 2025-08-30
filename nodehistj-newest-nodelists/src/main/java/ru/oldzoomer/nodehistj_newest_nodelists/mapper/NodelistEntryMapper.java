aa77a67db2ff5e78b68e92c60e30ae798efca65f
Egor Gavrilov <gavrilovegor519-2@yandex.ru>
2025-08-30 14:54:37 +0300

    Add comprehensive documentation to NodelistEntryMapper interface

--- nodehistj-newest-nodelists/src/main/java/ru/oldzoomer/nodehistj_newest_nodelists/mapper/NodelistEntryMapper.java
+++ nodehistj-newest-nodelists/src/main/java/ru/oldzoomer/nodehistj_newest_nodelists/mapper/NodelistEntryMapper.java
@@ -7,7 +7,17 @@ import org.mapstruct.ReportingPolicy;
 import ru.oldzoomer.nodehistj_newest_nodelists.dto.NodelistEntryDto;
 import ru.oldzoomer.nodehistj_newest_nodelists.entity.NodelistEntry;
 
+/**
+ * Mapper interface for converting NodelistEntry entities to NodelistEntryDto objects.
+ * Uses Spring component model and ignores unmapped target properties.
+ */
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
 public interface NodelistEntryMapper {
+    /**
+     * Converts a single NodelistEntry entity to a NodelistEntryDto object.
+     *
+     * @param nodelistEntry the NodelistEntry entity to convert
+     * @return the converted NodelistEntryDto object
+     */
     NodelistEntryDto toDto(NodelistEntry nodelistEntry);
 }
