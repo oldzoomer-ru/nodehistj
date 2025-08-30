7ed6738eaa33e634b8310cece16d537532cc5dd1
Egor Gavrilov <gavrilovegor519-2@yandex.ru>
2025-08-30 14:54:35 +0300

    Add NotFoundException class with comprehensive documentation

--- lib/common-utils/src/main/java/ru/oldzoomer/common/utils/NotFoundException.java
+++ lib/common-utils/src/main/java/ru/oldzoomer/common/utils/NotFoundException.java
@@ -1,6 +1,17 @@
 package ru.oldzoomer.common.utils;
 
+/**
+ * Exception thrown when a requested resource is not found.
+ * This exception is typically used to indicate that a requested resource
+ * does not exist in the system.
+ */
 public class NotFoundException extends RuntimeException {
+    /**
+     * Constructs a new NotFoundException with the specified detail message.
+     *
+     * @param message the detail message (which is saved for later retrieval
+     *               by the {@link #getMessage()} method)
+     */
     public NotFoundException(String message) {
         super(message);
     }
