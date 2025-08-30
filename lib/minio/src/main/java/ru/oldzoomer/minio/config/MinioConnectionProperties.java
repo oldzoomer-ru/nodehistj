2c4cc2a6b2bc4ffdaf53fdc3d5e997ef1e664b4d
Egor Gavrilov <gavrilovegor519-2@yandex.ru>
2025-08-30 14:54:35 +0300

    Enhance MinioConnectionProperties with documentation

--- lib/minio/src/main/java/ru/oldzoomer/minio/config/MinioConnectionProperties.java
+++ lib/minio/src/main/java/ru/oldzoomer/minio/config/MinioConnectionProperties.java
@@ -5,11 +5,26 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+/**
+ * Configuration properties for MinIO connection.
+ * This class holds the properties required to connect to a MinIO server.
+ */
 @ConfigurationProperties(prefix = "minio")
 @RequiredArgsConstructor
 @Getter
 public class MinioConnectionProperties {
+    /**
+     * The URL of the MinIO server.
+     */
     private final String url;
+
+    /**
+     * The access key for the MinIO server.
+     */
     private final String accessKey;
+
+    /**
+     * The secret key for the MinIO server.
+     */
     private final String secretKey;
 }
