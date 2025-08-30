c4c2cb57c011a3a201b6232f0ed470d4025c16f2
Egor Gavrilov <gavrilovegor519-2@yandex.ru>
2025-08-30 14:54:36 +0300

    Add comprehensive documentation to NodelistEntryRepository interface

--- nodehistj-history-diff/src/main/java/ru/oldzoomer/nodehistj_history_diff/repo/NodelistEntryRepository.java
+++ nodehistj-history-diff/src/main/java/ru/oldzoomer/nodehistj_history_diff/repo/NodelistEntryRepository.java
@@ -4,6 +4,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import ru.oldzoomer.nodehistj_history_diff.entity.NodelistEntry;
 
+/**
+ * Repository interface for NodelistEntry entities.
+ * Provides methods to find NodelistEntry entities based on various criteria.
+ */
 public interface NodelistEntryRepository extends JpaRepository<NodelistEntry, Long> {
+    /**
+     * Checks if a NodelistEntry entity exists based on the nodelist year and name.
+     *
+     * @param nodelistYear the year of the nodelist
+     * @param nodelistName the name of the nodelist
+     * @return true if the NodelistEntry entity exists, false otherwise
+     */
     boolean existsByNodelistYearAndNodelistName(Integer nodelistYear, String nodelistName);
 }
