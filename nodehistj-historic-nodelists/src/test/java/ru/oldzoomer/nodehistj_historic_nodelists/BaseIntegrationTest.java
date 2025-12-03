ff48dfd0147b216469e13ca767110a6381a9d4af
Egor Gavrilov <gavrilovegor519-2@yandex.ru>
2025-12-03 21:54:31 +0300

    Return PostgreSQL Testcontainer for tests

--- nodehistj-historic-nodelists/src/test/java/ru/oldzoomer/nodehistj_historic_nodelists/BaseIntegrationTest.java
+++ nodehistj-historic-nodelists/src/test/java/ru/oldzoomer/nodehistj_historic_nodelists/BaseIntegrationTest.java
@@ -11,6 +11,7 @@ import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.MinIOContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -28,6 +29,14 @@ import java.util.List;
 @Transactional
 @ActiveProfiles("test")
 public abstract class BaseIntegrationTest {
+
+    @SuppressWarnings("resource")
+    @Container
+    public static final PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:alpine")
+            .withDatabaseName("testdb")
+            .withUsername("testuser")
+            .withPassword("testpass")
+            .waitingFor(Wait.forListeningPort());
 
     @Container
     public static final RedpandaContainer redpandaContainer = new RedpandaContainer("redpandadata/redpanda")
@@ -51,7 +60,9 @@ public abstract class BaseIntegrationTest {
 
     @DynamicPropertySource
     static void registerProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", () -> "jdbc:h2:mem:nodelistj;DB_CLOSE_DELAY=-1");
+        registry.add("spring.datasource.url", () -> "jdbc:tc:postgresql:alpine:///testdb");
+        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
         registry.add("minio.url", minioContainer::getS3URL);
         registry.add("minio.accessKey", minioContainer::getUserName);
         registry.add("minio.secretKey", minioContainer::getPassword);
