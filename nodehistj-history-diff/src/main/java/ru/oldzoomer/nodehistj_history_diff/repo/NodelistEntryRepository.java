56f44d49abf7ffc6736b7164727a408b2d6ec01a
Egor Gavrilov <gavrilovegor519-2@yandex.ru>
2025-12-08 00:03:04 +0300

    Additional clean up unneeded custom queries

--- nodehistj-history-diff/src/main/java/ru/oldzoomer/nodehistj_history_diff/repo/NodelistEntryRepository.java
+++ nodehistj-history-diff/src/main/java/ru/oldzoomer/nodehistj_history_diff/repo/NodelistEntryRepository.java
@@ -1,6 +1,5 @@
 package ru.oldzoomer.nodehistj_history_diff.repo;
 
-import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import ru.oldzoomer.nodehistj_history_diff.entity.NodelistEntry;
@@ -18,12 +17,5 @@ public interface NodelistEntryRepository
      * @param nodelistName the name of the nodelist
      * @return true if the NodelistEntry entity exists, false otherwise
      */
-    @Query("""
-            SELECT EXISTS(
-                SELECT 1 FROM nodelist_entry nl
-                WHERE nl.nodelist_year = :nodelistYear
-                AND nl.nodelist_name = :nodelistName
-            )
-            """)
     boolean existsByNodelistYearAndNodelistName(Integer nodelistYear, String nodelistName);
 }
