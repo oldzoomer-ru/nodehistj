ded00936e1818ff74dd6319a70cca6924a05fa47
Egor Gavrilov <gavrilovegor519-2@yandex.ru>
2025-12-31 01:02:11 +0300

    Add dedicated exception for duplicate entries

---
+++ nodehistj-newest-nodelists/src/main/java/ru/oldzoomer/nodehistj_newest_nodelists/exception/DuplicateEntryException.java
@@ -0,0 +1,7 @@
+package ru.oldzoomer.nodehistj_newest_nodelists.exception;
+
+public class DuplicateEntryException extends IllegalStateException {
+    public DuplicateEntryException(String msg) {
+        super(msg);
+    }
+}
