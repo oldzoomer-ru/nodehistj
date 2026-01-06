de7eea5d51fa18b9e059beeeca962e94170b386d
Egor Gavrilov <gavrilovegor519-2@yandex.ru>
2026-01-06 05:56:02 +0300

    Use default Spring error handling

--- nodehistj-history-diff/src/main/java/ru/oldzoomer/nodehistj_history_diff/exception/DuplicateEntryException.java
+++
@@ -1,7 +0,0 @@
-package ru.oldzoomer.nodehistj_history_diff.exception;
-
-public class DuplicateEntryException extends IllegalStateException {
-    public DuplicateEntryException(String msg) {
-        super(msg);
-    }
-}
--- nodehistj-newest-nodelists/src/main/java/ru/oldzoomer/nodehistj_newest_nodelists/exception/DuplicateEntryException.java
+++
@@ -1,7 +0,0 @@
-package ru.oldzoomer.nodehistj_newest_nodelists.exception;
-
-public class DuplicateEntryException extends IllegalStateException {
-    public DuplicateEntryException(String msg) {
-        super(msg);
-    }
-}
