aac63e756a3c3a4ec4d8257da69d3203e2d83919
Egor Gavrilov <gavrilovegor519-2@yandex.ru>
2026-02-15 14:19:40 +0300

    Use more adequate method of disabling nodelist downloading at tests.

--- nodehistj-download-nodelists/src/main/java/ru/oldzoomer/nodehistj_download_nodelists/util/RunNodelistUpdate.java
+++ nodehistj-download-nodelists/src/main/java/ru/oldzoomer/nodehistj_download_nodelists/util/RunNodelistUpdate.java
@@ -3,12 +3,12 @@ package ru.oldzoomer.nodehistj_download_nodelists.util;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.context.annotation.Profile;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBooleanProperty;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-@Profile("!test")
+@ConditionalOnBooleanProperty(name = "app.updateAtStart", matchIfMissing = true)
 public class RunNodelistUpdate implements ApplicationRunner {
     private final UpdateNodelists updateNodelists;
 
