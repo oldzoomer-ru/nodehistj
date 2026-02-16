4f9c88265a696b48372a07ac21e9ef1b58b1dd6f
Egor Gavrilov <gavrilovegor519-2@yandex.ru>
2026-02-16 21:06:23 +0300

    Enable @NullMarked at nodelist download initial runner

--- nodehistj-download-nodelists/src/main/java/ru/oldzoomer/nodehistj_download_nodelists/util/RunNodelistUpdate.java
+++ nodehistj-download-nodelists/src/main/java/ru/oldzoomer/nodehistj_download_nodelists/util/RunNodelistUpdate.java
@@ -1,6 +1,7 @@
 package ru.oldzoomer.nodehistj_download_nodelists.util;
 
 import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.NullMarked;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBooleanProperty;
@@ -13,6 +14,7 @@ public class RunNodelistUpdate implements ApplicationRunner {
     private final UpdateNodelists updateNodelists;
 
     @Override
+    @NullMarked
     public void run(ApplicationArguments args) {
         updateNodelists.updateNodelists();
     }
