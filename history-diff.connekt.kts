aaf4a720ec7e63ff7e213c5ee999c166532fee6b
Egor Gavrilov <gavrilovegor519-2@yandex.ru>
2026-08-30 20:56:46 +0300

    refactor: replace hardcoded ports with variables
    Update HTTP client scripts to use configurable ports instead of hardcoded values.

--- history-diff.connekt.kts
+++ history-diff.connekt.kts
@@ -1,24 +1,26 @@
-GET("http://localhost:8081/diff/history") {
+val port = 8082
+
+GET("http://localhost:$port/diff/history") {
 
 }
-GET("http://localhost:8081/diff/history?zone=2") {
+GET("http://localhost:$port/diff/history?zone=2") {
 
 }
-GET("http://localhost:8081/diff/history?zone=2&network=5015") {
+GET("http://localhost:$port/diff/history?zone=2&network=5015") {
 
 }
-GET("http://localhost:8081/diff/history?zone=2&network=5015&node=519") {
+GET("http://localhost:$port/diff/history?zone=2&network=5015&node=519") {
 
 }
-GET("http://localhost:8081/diff/history?zone=2&page=1&size=10") {
+GET("http://localhost:$port/diff/history?zone=2&page=1&size=10") {
 
 }
-GET("http://localhost:8081/diff/history?size=50") {
+GET("http://localhost:$port/diff/history?size=50") {
 
 }
-GET("http://localhost:8081/diff/history?zone=50000") {
+GET("http://localhost:$port/diff/history?zone=50000") {
 
 }
-GET("http://localhost:8081/diff/history?zone=999&page=0&size=20") {
+GET("http://localhost:$port/diff/history?zone=999&page=0&size=20") {
 
 }
