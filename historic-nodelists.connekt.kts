45de2f00211d267cd5b69065941a97c8830a2173
Egor Gavrilov <gavrilovegor519-2@yandex.ru>
2026-08-30 14:57:05 +0300

    test: add HTTP client scripts for historic services
    Add .http and .connekt.kts files for historic-nodelists and history-diff services.
    Includes GET requests with various query parameters for local debugging and API validation.

---
+++ historic-nodelists.connekt.kts
@@ -0,0 +1,24 @@
+GET("http://localhost:8080/historic/historicNodelist?year=2024&dayOfYear=180") {
+
+}
+GET("http://localhost:8080/historic/historicNodelist?year=2024&dayOfYear=180&zone=2") {
+
+}
+GET("http://localhost:8080/historic/historicNodelist?year=2024&dayOfYear=180&zone=2&network=5015") {
+
+}
+GET("http://localhost:8080/historic/historicNodelist?year=2024&dayOfYear=180&zone=2&network=5015&node=519") {
+
+}
+GET("http://localhost:8080/historic/historicNodelist?year=2025&dayOfYear=1&zone=255") {
+
+}
+GET("http://localhost:8080/historic/historicNodelist?year=2024&dayOfYear=400") {
+
+}
+GET("http://localhost:8080/historic/historicNodelist?year=2024&dayOfYear=180&zone=50000") {
+
+}
+GET("http://localhost:8080/historic/historicNodelist?year=2030&dayOfYear=1") {
+
+}
---
+++ history-diff.connekt.kts
@@ -0,0 +1,24 @@
+GET("http://localhost:8081/diff/history") {
+
+}
+GET("http://localhost:8081/diff/history?zone=2") {
+
+}
+GET("http://localhost:8081/diff/history?zone=2&network=5015") {
+
+}
+GET("http://localhost:8081/diff/history?zone=2&network=5015&node=519") {
+
+}
+GET("http://localhost:8081/diff/history?zone=2&page=1&size=10") {
+
+}
+GET("http://localhost:8081/diff/history?size=50") {
+
+}
+GET("http://localhost:8081/diff/history?zone=50000") {
+
+}
+GET("http://localhost:8081/diff/history?zone=999&page=0&size=20") {
+
+}
